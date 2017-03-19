abf2c732cede0e238c2071f65bd541d7d405ab17
Thomas LECLERCQ <degnus@gmail.com>
2017-03-20 05:23:17 +0530

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,11 @@ fi
 if [ -n "${OWNCLOUD_CRED+set}" ]; then
   URL="http://c.thomas.sh/remote.php/webdav/public/${REPO_NAME}"
   
-  if [ -n "${TRAVIS_TAG+set}" ]; then
+  if [ -n "${TRAVIS_TAG+set}" ] && [ -z "${TRAVIS_TAG}"]; then
     curl -X PUT -u "${OWNCLOUD_CRED}" "${URL}/release-latest.zip" --data-binary @"${FILE_PATH}"
     curl -X PUT -u "${OWNCLOUD_CRED}" "${URL}/release-${TRAVIS_TAG}.zip" --data-binary @"${FILE_PATH}"
   else
     curl -X PUT -u "${OWNCLOUD_CRED}" "${URL}/beta-latest.zip" --data-binary @"${FILE_PATH}"
-    curl -X PUT -u "${OWNCLOUD_CRED}" "${URL}/beta-${TRAVIS_TAG}.zip" --data-binary @"${FILE_PATH}"
+    curl -X PUT -u "${OWNCLOUD_CRED}" "${URL}/beta-${TRAVIS_COMMIT}.zip" --data-binary @"${FILE_PATH}"
   fi
 fi
