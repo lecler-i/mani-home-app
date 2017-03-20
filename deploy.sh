790361863a9889c6beca5038eb4199f968ad8b9b
Thomas LECLERCQ <degnus@gmail.com>
2017-03-20 09:11:51 +0530

    Fix deploy script (if conditions + extension)

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,21 @@ REPO_NAME=`echo $TRAVIS_REPO_SLUG |  cut -d \/ -f 2`
 FILE_PATH="android/app/build/outputs/apk/app-release.apk"
 
 # Code push
-if [ -n "${CODE_PUSH_TOKEN+set}" ]; then
+if [ ! -z "${CODE_PUSH_TOKEN}" ]; then
 	./node_modules/.bin/code-push login --accessKey "${CODE_PUSH_TOKEN}" 
 	./node_modules/.bin/code-push release-react usift-app android
 	./node_modules/.bin/code-push logout
 fi
 
 # OwnCloud
-if [ -n "${OWNCLOUD_CRED+set}" ]; then
+if [ ! -z "${OWNCLOUD_CRED}" ]; then
   URL="http://c.thomas.sh/remote.php/webdav/public/${REPO_NAME}"
   
-  if [ -n "${TRAVIS_TAG+set}" ] && [ -z "${TRAVIS_TAG}"]; then
-    curl -X PUT -u "${OWNCLOUD_CRED}" "${URL}/release-latest.zip" --data-binary @"${FILE_PATH}"
-    curl -X PUT -u "${OWNCLOUD_CRED}" "${URL}/release-${TRAVIS_TAG}.zip" --data-binary @"${FILE_PATH}"
+  if [ ! -z "${TRAVIS_TAG}" ]; then
+    curl -X PUT -u "${OWNCLOUD_CRED}" "${URL}/release-latest.apk" --data-binary @"${FILE_PATH}"
+    curl -X PUT -u "${OWNCLOUD_CRED}" "${URL}/release-${TRAVIS_TAG}.apk" --data-binary @"${FILE_PATH}"
   else
-    curl -X PUT -u "${OWNCLOUD_CRED}" "${URL}/beta-latest.zip" --data-binary @"${FILE_PATH}"
-    curl -X PUT -u "${OWNCLOUD_CRED}" "${URL}/beta-${TRAVIS_COMMIT}.zip" --data-binary @"${FILE_PATH}"
+    curl -X PUT -u "${OWNCLOUD_CRED}" "${URL}/beta-latest.apk" --data-binary @"${FILE_PATH}"
+    curl -X PUT -u "${OWNCLOUD_CRED}" "${URL}/beta-${TRAVIS_COMMIT}.apk" --data-binary @"${FILE_PATH}"
   fi
 fi
